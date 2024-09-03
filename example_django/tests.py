bc7220cc2402e401fa9e1f1922f39394dec2a10e
Ibrahim H <bitsnaps@yahoo.fr>
2024-09-03 09:34:31 +0100

    test: simplest case

---
+++ example_django/tests.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+
+# Create your tests here.
+class AppTest(SimpleTestCase):
+
+    # function name must starts with "test"
+    def test_endpoint_is_resolved(self):
+        assert 1 == 2
